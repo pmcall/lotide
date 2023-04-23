9b266aa4277bd454a713281ee42fbf97c727ebb3
Pabs <paul.mca.ca@gmail.com>
2023-04-23 01:58:08 +0000

    Added completed assertEqual.js file which compares two inputs and asserts if they are equal or not. Message has improved readibility by using emojis

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
