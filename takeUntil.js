676657c2b6cfd37c3bd7500a311a07b4829a569a
Pabs <paul.mca.ca@gmail.com>
2023-04-30 16:29:36 +0000

    Adding takeUntil.js which takes in an array and condition (as a callback) and returns the start of the array until the point where the condition is met.

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const takeUntil = function(array, callback) {
+  let spliceEnd = array.findIndex(callback)
+  console.log(spliceEnd)
+  return array.slice(0, spliceEnd)
+}
+
+
+//INPUTS
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+//TESTS
+
+const assertArraysEqual = function(array1, array2) {
+  let booleanSwitch = true;
+  if (array1.length !== array2.length) {
+    booleanSwitch = false;
+  }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        booleanSwitch = false;
+      } 
+    }
+    if (booleanSwitch === true) {
+      console.log(`I compared ${array1} with ${array2}. I found that both arrays are exactly the same!`) 
+    } else {
+      console.log(`I compared ${array1} with ${array2}. It seems like they are different.`) 
+    }
+};
+
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"])
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2])
