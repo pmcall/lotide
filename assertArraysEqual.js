b65c6c970543a9254ffd60f8a22ef2eeca465103
Pabs <paul.mca.ca@gmail.com>
2023-05-11 02:56:54 +0000

    Cleaned up assertArraysEqual.js and test files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function (array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
-    console.log(`✅✅✅ These arrays match! "${array1.join(", ")}" is the same as "${array2.join(", ")}"`)
+    console.log(`✅✅✅ These arrays match! "${array1.join(", ")}" is the same as "${array2.join(", ")}"`);
   } else {
-    console.log(`🛑🛑🛑 These arrays do not match! "${array1.join(", ")}" is not the same as "${array2.join(", ")}"`)
+    console.log(`🛑🛑🛑 These arrays do not match! "${array1.join(", ")}" is not the same as "${array2.join(", ")}"`);
   }
-}
+};
 
 module.exports = assertArraysEqual;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,14 +2,14 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 
 // TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 3, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 3, 3], [1, 2, 3]);
 assertArraysEqual(["Apple", "Banana", "Cabbage"], ["Apple", "Banana", "Cabbage"]), true;
 assertArraysEqual(["Apple", "Banana", "Cabbage"], ["Watercress", "Peach", "Sausage"]), false;
 assertArraysEqual([],[]),true;//PASS
 assertArraysEqual([1,2,3], [1,2]),false;//FAIL
 assertArraysEqual([],["banana"]), false;
-assertArraysEqual(["hello", "cheese"], ["hello", "cheese"])
-assertArraysEqual(["bananas"], ["hello", 1])
-assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
-assertArraysEqual([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])
+assertArraysEqual(["hello", "cheese"], ["hello", "cheese"]);
+assertArraysEqual(["bananas"], ["hello", 1]);
+assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
+assertArraysEqual([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]);
